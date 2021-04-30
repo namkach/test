13662e287af34ba3a44fe18e74672a3febc9ac1e
SunitaKac <nam.nazoru504@gmail.com>
2021-04-30 13:06:25 +0700

    change url

--- cypress/integration/examples/main.js
+++ cypress/integration/examples/main.js
@@ -1,12 +1,12 @@
 describe('test', () => {
-    var arr = ['qwerty', 'yui', 'asd']
+    var arr = ['qwerty', 'yui']
     arr.forEach((text) => {
         it('test 1 ' + text, () => { 
             Cypress.on('uncaught:exception', (err, runnable) => {
                 return false
             })
 
-            cy.visit('https://integ1.7online.7eleven.co.th/')
+            cy.visit('https://www.pixiv.net/en/')
             cy.log('text : ' + text)
         })
     })
