2e5677fdd0169ef904ff05cfc150f57c0adaa053
SunitaKac <nam.nazoru504@gmail.com>
2021-04-30 12:54:26 +0700

    edit file

--- cypress/integration/examples/main.js
+++ cypress/integration/examples/main.js
@@ -1,11 +1,11 @@
 describe('test', () => {
     var arr = ['qwerty', 'yui', 'asd']
     arr.forEach((text) => {
-        it('test ' + text, () => { 
+        it('test 1 ' + text, () => { 
             Cypress.on('uncaught:exception', (err, runnable) => {
                 return false
             })
-            
+
             cy.visit('https://integ1.7online.7eleven.co.th/')
             cy.log('text : ' + text)
         })
